c771453f774fab51069734115e9fc87ca6d08416
paularinzee <arinzennajipual@gmail.com>
2023-04-09 16:07:22 +0100

    first commit

---
+++ portfolio.ts
@@ -0,0 +1,328 @@
+import emoji from 'react-easy-emoji';
+import {
+  EducationType,
+  ExperienceType,
+  FeedbackType,
+  ProjectType,
+  SkillsSectionType,
+  SkillBarsType,
+  SEODataType,
+  SocialLinksType,
+  GreetingsType,
+} from './types/sections';
+
+export const greetings: GreetingsType = {
+  name: 'Paul Nnaji',
+  title: "Hi all, I'm Paul",
+  description:
+    "I'm passionate DevOps engineer with a strong background in software development, with a focus on Python,Django,laravel and JavaScript, as well as expertise in cloud technologies such as Amazon Web Services (AWS),Microsoft Azure and Google Cloud..",
+  resumeLink:
+  '#'
+    // 'https://docs.google.com/document/d/1_4RIfbyt4FFHW_bNplnoINQr01neTu7XDeeUlT_2O_k/edit?usp=sharing',
+};
+
+export const openSource = {
+  githubUserName: 'paularinzee',
+};
+
+export const contact = {};
+
+export const socialLinks: SocialLinksType = {
+  url: '#',
+  linkedin: 'https://www.linkedin.com/in/paularinze/',
+  github: 'https://github.com/paularinzee',
+  // instagram: 'https://www.instagram.com/__hanzla100',
+  // facebook: 'https://www.facebook.com/1hanzla100',
+  twitter: 'https://twitter.com/PaulArinzeyoung',
+};
+
+export const skillsSection: SkillsSectionType = {
+  title: 'What I do',
+  subTitle: 'CLOUD DEVOPS ENGINEER WHO WANTS TO EXPLORE EVERY CLOUD',
+  data: [
+    {
+      title: 'Backend Development',
+      lottieAnimationFile: '/lottie/skills/fullstack.json', // Path of Lottie Animation JSON File
+      skills: [
+        emoji(
+          '⚡ Building RESTful APIs in Adonis.js'
+        ),
+        emoji('⚡ Building Web Application & APIs in Laravel'),
+        emoji('⚡ Building RESTful APIs in Django & Django REST Framework'),
+      ],
+      softwareSkills: [
+        {
+          skillName: 'HTML-5',
+          fontAwesomeClassName: 'vscode-icons:file-type-html',
+        },
+        {
+          skillName: 'CSS-3',
+          fontAwesomeClassName: 'vscode-icons:file-type-css',
+        },
+        {
+          skillName: 'JavaScript',
+          fontAwesomeClassName: 'logos:javascript',
+        },
+        {
+          skillName: 'Python',
+          fontAwesomeClassName: 'logos:python',
+        },
+        {
+          skillName: 'Django',
+          fontAwesomeClassName: 'vscode-icons:file-type-django',
+        },
+        {
+          skillName: 'Laravel',
+          fontAwesomeClassName: 'vscode-icons:laravel',
+        },
+
+        {
+          skillName: 'Redux',
+          fontAwesomeClassName: 'logos:redux',
+        },
+        {
+          skillName: 'NPM',
+          fontAwesomeClassName: 'logos:npm-icon',
+        },
+        {
+          skillName: 'Yarn',
+          fontAwesomeClassName: 'logos:yarn',
+        },
+      ],
+    },
+    {
+      title: 'Cloud Infra-Architecture and DevOps Tools',
+      lottieAnimationFile: '/lottie/skills/cloudinfra.json', // Path of Lottie Animation JSON File
+      skills: [
+        emoji('⚡ Experience of working on multiple cloud platforms'),
+        emoji(
+          '⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases'
+        ),
+        emoji(
+          '⚡ Building CI/CD pipelines for automated testing & deployment using Github Actions, Jenkins, Circle CI, Cloud Formation & Travis CI '
+        ),
+        emoji(
+        '⚡ Designing and implementing automation frameworks to streamline the software development process'
+        ),
+      ],
+      softwareSkills: [
+        // ? Check README To get icon details
+        {
+          skillName: 'AWS',
+          fontAwesomeClassName: 'logos:aws',
+        },
+        {
+          skillName: 'Azure',
+          fontAwesomeClassName: 'logos:microsoft-azure',
+        },
+        {
+          skillName: 'Heroku',
+          fontAwesomeClassName: 'logos:heroku-icon',
+        },
+        {
+          skillName: 'PostgreSQL',
+          fontAwesomeClassName: 'logos:postgresql',
+        },
+        {
+          skillName: 'Github',
+          fontAwesomeClassName: 'akar-icons:github-fill',
+        },
+        {
+          skillName: 'Docker',
+          fontAwesomeClassName: 'logos:docker-icon',
+        },
+        {
+          skillName: 'Github Actions',
+          fontAwesomeClassName: 'logos:github-actions',
+        },
+        
+        {
+          skillName: 'Nginx',
+          fontAwesomeClassName: 'logos:nginx',
+        },
+
+        {
+          skillName: 'Jenkins',
+          fontAwesomeClassName: 'logos:jenkins',
+        },
+        {
+          skillName: 'Kubernetes',
+          fontAwesomeClassName: 'logos:kubernetes',
+        },
+        {
+          skillName: 'Ansible',
+          fontAwesomeClassName: 'logos:ansible',
+        },
+        {
+          skillName: 'Terraform',
+          fontAwesomeClassName: 'logos:terraform',
+        },
+        {
+          skillName: 'ArgoCD',
+          fontAwesomeClassName: 'argocd:',
+        },
+        
+        
+      ],
+    },
+    // {
+    //   title: 'Blockchain',
+    //   lottieAnimationFile: '/lottie/skills/ethereum.json', // Path of Lottie Animation JSON File
+    //   skills: [
+    //     emoji(
+    //       '⚡ Experience in developing Smart Contract using Solidity & Ethereum'
+    //     ),
+    //     emoji(
+    //       '⚡ Building Scripts for automated testing & deployment of Smart Contracts using Brownie & Infura'
+    //     ),
+    //     emoji(
+    //       '⚡ Experience of using Openzeppelin Smart Contract Standards & Chainlink Oracles'
+    //     ),
+    //     emoji('⚡ Developing NFT Smart Contracts using ERC-721 Token Standard'),
+    //     emoji(
+    //       '⚡ Building Dapps with React.js & Solidity using Web3.js, Moralis & IPFS'
+    //     ),
+    //   ],
+    //   softwareSkills: [
+    //     {
+    //       skillName: 'Ethereum',
+    //       fontAwesomeClassName: 'logos:ethereum',
+    //     },
+    //     {
+    //       skillName: 'Solidity',
+    //       fontAwesomeClassName: 'logos:solidity',
+    //     },
+    //     {
+    //       skillName: 'Web3js',
+    //       fontAwesomeClassName: 'logos:web3js',
+    //     },
+    //     {
+    //       skillName: 'Metamask',
+    //       fontAwesomeClassName: 'logos:metamask-icon',
+    //     },
+    //     {
+    //       skillName: 'Ganache',
+    //       fontAwesomeClassName: 'logos:ganache-icon',
+    //     },
+    //   ],
+    // },
+  ],
+};
+
+export const SkillBars: SkillBarsType[] = [
+  {
+    Stack: 'DevOps', //Insert stack or technology you have experience in
+    progressPercentage: '90', //Insert relative proficiency in percentage
+  },
+  {
+    Stack: 'Backend',
+    progressPercentage: '90',
+  },
+  {
+    Stack: 'Programming',
+    progressPercentage: '90',
+  },
+];
+
+export const educationInfo: EducationType[] = [
+  {
+    schoolName: 'Michael Okpara University of Agriculture Umudike',
+    subHeader: 'Master of Science in Computer Science',
+    
+   
+  },
+];
+
+export const experience: ExperienceType[] = [
+  {
+    role: 'Frontend Developer',
+    company: 'Duseca Software',
+    companyLogo: '/img/icons/common/dusecaSoftware.jpg',
+    date: 'Apr 2022 – Jun 2022',
+    desc: 'I worked as frontend web developer to design web based admin dashboards for mulitple apps using Django, Bootstrap, jQuery, Chart.js and some other libraries.',
+  },
+  {
+    role: 'API Engineer',
+    company: 'Duseca Software',
+    companyLogo: '/img/icons/common/dusecaSoftware.jpg',
+    date: 'Jan 2022 – Mar 2022',
+    desc: 'I worked as API Engineer on a Kale App. Kale is a community platform for the creation of social reviews based on verified transactions. I built Fully Optimized and High Performance RESTful APIs using Django, DjangoRESTFramework, PostgresSQL, JWT Authentication, AWS, Integrated Plaid APIs for Fetching Bank Transactions, Integrated Sentry for monitoring Django Server and deployed the API on Heroku Server.',
+    // descBullets: [
+    // 	"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
+    // 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    // ],
+  },
+  {
+    role: 'Full Stack Developer',
+    company: 'Bleed-AI',
+    companyLogo: '/img/icons/common/bleedAI.jpg',
+    date: 'Sept 2021 - Oct 2021',
+    desc: 'Worked as django web developer to create a website in which I integrated computer vision AI model (built by the team of BleedAI) to process YouTube URLs and show Real Time processing on Website using Django Channels WebSockets , threading for running multiple instances of AI Model, jQuery for UI rendering and deployed it on Heroku server.',
+  },
+  {
+    role: 'Backend Developer',
+    company: 'Wapidu',
+    companyLogo: '/img/icons/common/wapidu.jpg',
+    date: 'Sept 2021',
+    desc: 'Worked as a Django Developer to integrate Stripe payment gateway in wapidu.com and Created REST APIs using Django REST Framework to integrate Django Stripe backend to Vue.js Frontend and Deployed it on Azure based Docker container registry.',
+  },
+];
+
+export const projects: ProjectType[] = [
+  {
+    name: 'Serverless-App-Project-in-AWS',
+    desc: 'The Project is to develop and deploy a simple "TODO" application using AWS Lambda and Serverless framework.',
+    github: 'https://github.com/paularinzee/Serverless-App-Project-in-AWS',
+    // link: 'https://github.com/paularinzee/Serverless-App-Project-in-AWS/',
+  },
+  {
+    name: 'Machine-learning-microservice-kubernetes',
+    desc: 'Deployed a Machine Learning microservices that are elastic and fault tolerant',
+    github: 'https://github.com/paularinzee/Machine-learning-microservice-kubernetes',
+  },
+  {
+    name: 'High-Availaibility-Web-App',
+    desc: 'Deploy a simple website on a public-private HA infrastructure',
+    github: 'https://github.com/paularinzee/Deploy-a-High-Availaibility-Web-App-Using-Cloud-Formation',
+  },
+  {
+    name: 'deploy-jenkins-pipeline-to-eks-with-terraform',
+    desc: ' End to end deployment of Applications to Kubernetes using a Jenkins CI/CD Pipeline',
+    github: 'https://github.com/paularinzee?tab=repositories',
+  },
+];
+
+export const feedbacks: FeedbackType[] = [
+  {
+    name: 'John Smith',
+    feedback:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.',
+  },
+  {
+    name: 'John Smith',
+    feedback:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.',
+  },
+];
+
+// option to hide or show the ContactUs component
+export const showContactUs: boolean = true;
+
+// See object prototype on /types/section.ts page
+export const seoData: SEODataType = {
+  title: 'Hanzla Tauqeer',
+  description:
+    'A passionate Full Stack Web Developer and Blockchain Developer.',
+  author: 'Hanzla Tauqeer',
+  image: 'https://avatars3.githubusercontent.com/u/59178380?v=4',
+  url: 'https://developer-portfolio-1hanzla100.vercel.app',
+  keywords: [
+    'Hanzla',
+    'Hanzla Tauqeer',
+    '@1hanzla100',
+    '1hanzla100',
+    'Portfolio',
+    'Hanzla Portfolio ',
+    'Hanzla Tauqeer Portfolio',
+  ],
+};
